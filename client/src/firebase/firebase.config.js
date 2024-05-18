a5ffb615017c2ed6cf7d0738e88ef5fbf1d0b09f
ProgSomel <somelahmed88@gmail.com>
2024-05-18 17:04:05 +0600

    Client setup done

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPSLcayy1GvaCd5gUUdjiexucSazhfQ20",
+  authDomain: "solo-space-50293.firebaseapp.com",
+  projectId: "solo-space-50293",
+  storageBucket: "solo-space-50293.appspot.com",
+  messagingSenderId: "142523027925",
+  appId: "1:142523027925:web:0f880feda450d3a3ff6d5c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/src/layouts/Main.jsx
@@ -0,0 +1,21 @@
+
+import { Outlet } from 'react-router-dom';
+import Navbar from './../components/Navbar';
+import Footer from './../components/Footer';
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+
+Main.propTypes = {
+
+};
+
+
+export default Main;
